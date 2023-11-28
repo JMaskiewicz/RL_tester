c4fcddf41e3b6714c74a262d40bd705a8c9fefd8
jmaskiew <109626411+jmaskiew@users.noreply.github.com>
2023-11-28 13:34:49 +0100

    DQN agent without gamma

--- models/DQN_torch.py
+++ models/DQN_torch.py
@@ -1,137 +1,191 @@
+"""
+Model description:
+    - Input: array of the last n closing prices
+    - Output: 1D array of the last n closing prices
+    - Action: 1, 0, -1
+
+DQN agent:
+    - looking only on next reward (gamma = 0)
+
+Backtesting
+    - playing with full capital
+    - no leverage
+"""
+
+# import libraries
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import numpy as np
+import torch.nn.functional as F
 from collections import deque
+import numpy as np
 import random
+import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-import time
 
 from data.function.load_data import load_data
 
+
+# Set seeds for reproducibility
+torch.manual_seed(0)
+np.random.seed(0)
+random.seed(0)
+
+class DQNetwork(nn.Module):
+    def __init__(self, state_dim, num_actions, architecture):
+        super(DQNetwork, self).__init__()
+        layers = []
+        for units in architecture:
+            layers.append(nn.Linear(state_dim, units))
+            layers.append(nn.ReLU())
+            state_dim = units
+
+        layers.append(nn.Dropout(0.1))
+        layers.append(nn.Linear(architecture[-1], num_actions))
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
 class DQNAgent:
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.memory = deque(maxlen=2000)
-        self.gamma = 0.95  # discount rate
-        self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.999
-        self.learning_rate = 0.0005
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        #self.device = torch.device("cpu")
-        print(f"Using device: {self.device}")
-        self.model = self.build_model().to(self.device)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
-        self.action_space = np.arange(-100, 101, 25)
+    def __init__(self, state_dim, num_actions, learning_rate, gamma, epsilon_start, epsilon_end, epsilon_decay_steps, epsilon_exponential_decay, replay_capacity, architecture, batch_size, variables):
+        self.state_dim = state_dim
+        self.num_actions = num_actions
+        self.gamma = gamma
+        self.epsilon = epsilon_start
+        self.epsilon_end = epsilon_end
+        self.epsilon_decay = (epsilon_start - epsilon_end) / epsilon_decay_steps
+        self.epsilon_exponential_decay = epsilon_exponential_decay
+        self.batch_size = batch_size
 
-    def build_model(self):
-        model = nn.Sequential(
-            nn.Linear(self.state_size, 2560),
-            nn.ReLU(),
-            nn.Linear(2560, 1280),
-            nn.ReLU(),
-            nn.Linear(1280, 640),
-            nn.ReLU(),
-            nn.Linear(640, self.action_size)
-        )
-        return model
+        self.device = torch.device("cpu") #torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
+        self.network = DQNetwork(state_dim, num_actions, architecture).to(self.device)
+        self.optimizer = optim.Adam(self.network.parameters(), lr=learning_rate)
+        self.experience = deque(maxlen=replay_capacity)
 
-    def act(self, state):
-        if not isinstance(state, torch.Tensor):
-            state = torch.from_numpy(state).float().to(self.device)
-        else:
-            state = state.float().to(self.device)
+        self.variables = variables
 
-        if np.random.rand() <= self.epsilon:
-            return np.random.choice(self.action_space)
-        self.model.eval()
-        with torch.no_grad():
-            act_values = self.model(state)
-        self.model.train()
-        return self.action_space[torch.argmax(act_values[0]).item()]
+    def epsilon_greedy_policy(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(-1, 2)
+        state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+        q_values = self.network(state).to(self.device)
+        action = np.argmax(q_values.detach().numpy()) - 1
+        return action
 
-    def replay(self, batch_size):
-        if len(self.memory) < batch_size:
+    def memorize_transition(self, s, a, r):
+        self.experience.append((s, a, r))
+
+    def replay(self):
+        if len(self.experience) < self.batch_size:
             return
-        minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            state = torch.from_numpy(state).float().to(self.device)
-            next_state = torch.from_numpy(next_state).float().to(self.device)
-            reward = torch.tensor(reward).float().to(self.device)
-            action_index = np.where(self.action_space == action)[0][0]
 
-            self.optimizer.zero_grad()
-            target = reward
-            if not done:
-                target = reward + self.gamma * torch.max(self.model(next_state).detach())
-            target_f = self.model(state)
-            target_f[0][action_index] = target
-            loss = nn.MSELoss()(target_f, target_f.clone().detach())
-            loss.backward()
-            self.optimizer.step()
+        # Sample a minibatch from the memory
+        minibatch = random.sample(self.experience, self.batch_size)
+        states, actions, rewards = map(np.array, zip(*minibatch))
 
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+        # Convert to PyTorch tensors
+        states = torch.FloatTensor(states).to(self.device)
+        actions = torch.LongTensor(actions).to(self.device)
+        rewards = torch.FloatTensor(rewards).to(self.device)
 
-    def load(self, name):
-        self.model.load_state_dict(torch.load(name))
+        # Compute Q values for current states
+        q_values = self.network(states)
+        q_values = q_values.squeeze(1)  # Remove the extra dimension
 
-    def save(self, name):
-        torch.save(self.model.state_dict(), name)
+        # Targets = r (gamma=0)
+        targets = rewards
+
+        q_values = q_values.gather(1, (actions + 1).unsqueeze(1)).squeeze(1)
+        loss = F.mse_loss(q_values, targets).to(self.device)
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        # Epsilon update for exploration-exploitation tradeoff
+        if self.epsilon > self.epsilon_end:
+            self.epsilon -= self.epsilon_decay
+        else:
+            self.epsilon *= self.epsilon_exponential_decay
+
 
 # Example usage
 currencies = ['EURUSD']
 df = load_data(currencies=currencies, timestamp_x='1D')
 df = df.dropna()
-start_date = '2020-01-01'
-split_date = '2022-01-01'
+start_date = '2005-01-01'
+split_date = '2020-01-01'
 df_train = df[start_date:split_date]
-df_train = df_train[['Close']]
-
 df_test = df[split_date:]
-df_test = df_test[['Close']]
 
+variables = ['Open', 'High', 'Low', 'Close']
 episodes = 1000
-action_size = len(np.arange(-100, 101, 25))
-batch_size = 32  # 32
-look_back = 20  # Number of previous observations to include
-state_size = df_train.shape[1] * look_back
-agent = DQNAgent(state_size, action_size)
+action_size = len(np.arange(-1, 2, 1))
+look_back = 50  # Number of previous observations to include
+state_size = len(variables) * look_back
 
-all_total_rewards = []
-episode_durations = []
-for e in range(episodes):
-    start_time = time.time()
-    print(f"Episode: {e}/{episodes}")
-    total_reward = 0
-    for t in tqdm(range(look_back, len(df_train) - 1)):
-        state = df_train.iloc[t - look_back:t].values.flatten().reshape(1, -1)
-        action = agent.act(state)
+agent = DQNAgent(
+    state_dim=state_size,
+    num_actions=action_size,
+    learning_rate=0.01,
+    gamma=0.99,
+    epsilon_start=1.0,
+    epsilon_end=0.25,
+    epsilon_decay_steps=100000,
+    epsilon_exponential_decay=0.9999,
+    replay_capacity=int(1e6),
+    architecture=(512, 512, 512, 512),
+    batch_size=4096,
+    variables=variables
+)
 
-        next_state = df_train.iloc[t - look_back + 1:t + 1].values.flatten().reshape(1, -1)
-        reward = (df_train.iloc[t + 1]['Close'] - df_train.iloc[t]['Close']) * action
-        done = t == len(df_train) - 2
+def train_agent(agent, df_train, episodes, look_back):
+    total_rewards = []
+    episode_durations = []
+    for episode in range(episodes):
+        state = get_state(df_train, look_back)
+        total_reward = 0
+        step = 0
+        while True:
+            if step >= look_back - 1:  # Ensure complete look-back window
+                action = agent.epsilon_greedy_policy(state)
+                reward, done = get_reward(df_train, state, action, step)
+                agent.memorize_transition(state, action, reward)
+                total_reward += reward
+                agent.replay()
+                if done:
+                    break
 
-        agent.remember(state, action, reward, next_state, done)
+            step += 1
+        total_rewards.append(total_reward)
+        episode_durations.append(step)
+        if episode % 1 == 0:
+            print('episode: ', episode + 1)
+            print("current epsilon: ", agent.epsilon)
+            print(f"Episode {episode + 1}: Total Reward: {total_reward}, Duration: {step}")
+            print('----')
 
-        # Perform experience replay if enough memory is gathered
-        agent.replay(batch_size)
 
-        state = next_state
-        total_reward += reward
+    return total_rewards, episode_durations
 
-        if done:
-            print(f"Episode: {e}/{episodes}, Total Reward: {total_reward}")
 
-    episode_duration = time.time() - start_time  # Calculate duration
-    episode_durations.append(episode_duration)
-    all_total_rewards.append(total_reward)
+def get_state(df, look_back):
+    data = df.iloc[:look_back][variables].values.flatten().reshape(1, -1)
+    return data
+
+def get_reward(df, state, action, t):
+    if t < len(df) - 1:
+        price_diff = (df.iloc[t + 1]['Close'] - df.iloc[t]['Close']) / df.iloc[t]['Close'] * 100
+        reward = action * price_diff.iloc[0]
+    else:
+        reward = 0
+    done = t == len(df) - 1
+    return reward, done
+
+all_total_rewards, episode_durations = train_agent(agent, df_train, episodes, look_back)
 
 # Plotting the results after all episodes
 plt.plot(all_total_rewards)
@@ -141,47 +195,96 @@ plt.ylabel('Total Reward')
 plt.show()
 
 
-# Ensure epsilon is set to 0 for greedy action selection
-#agent.epsilon = 0.5
-# TEST
-# Initialize an empty list to store the predicted actions
-predicted_actions = []
+def calculate_win_rate(df, currency):
+    # Calculate price change
+    df[('Price_Change', currency)] = df[('Close', currency)].diff()
 
-# Loop over the DataFrame to make predictions for each state
-for i in range(look_back, len(df_test)):
-    # Prepare the state from the current position minus look_back
-    current_state = df_test.iloc[i - look_back:i].values.flatten().reshape(1, -1)
+    # Define win and loss conditions
+    win_condition = ((df[('Price_Change', currency)] > 0) & (df[('Action', currency)] == 1)) | \
+                    ((df[('Price_Change', currency)] < 0) & (df[('Action', currency)] == -1))
 
-    # Convert the state to PyTorch tensor and transfer to the device (GPU/CPU)
-    current_state_tensor = torch.from_numpy(current_state).float().to(agent.device)
+    loss_condition = ((df[('Price_Change', currency)] > 0) & (df[('Action', currency)] == -1)) | \
+                     ((df[('Price_Change', currency)] < 0) & (df[('Action', currency)] == 1))
 
-    # Predict the action using the trained model
-    action = agent.act(current_state_tensor)
+    # Count wins and losses
+    wins = df[win_condition].shape[0]
+    losses = df[loss_condition].shape[0]
+    win_rate = wins / (wins + losses) if (wins + losses) > 0 else 0
+    number_of_trades = wins + losses
 
-    # Append the predicted action to the list
-    predicted_actions.append(action)
+    return win_rate, number_of_trades
 
-# Since we start predicting after 'look_back' rows, we need to prepend NaNs or a placeholder
-# to align the length of the predictions with the DataFrame
-padding = [None] * look_back
-full_predictions = padding + predicted_actions
+def make_predictions_with_agent(agent, df, look_back, currency):
+    predicted_actions = [None] * look_back  # Initialize with None for initial timestamps
 
-# Add the predicted actions to the DataFrame as a new column
-df_test['Predicted_Action'] = full_predictions
+    for t in tqdm(range(len(df) - look_back)):
+        state = get_state(df[variables].iloc[t:t + look_back], look_back)
+        action = agent.epsilon_greedy_policy(state)
+        predicted_actions.append(action)
 
-# TRAIN
-predicted_actions = []
+    # Ensure the predicted actions list matches the DataFrame length
+    predicted_actions.extend([None] * (len(df) - len(predicted_actions)))
 
-for i in range(look_back, len(df_train)):
-    current_state = df_train.iloc[i - look_back:i].values.flatten().reshape(1, -1)
+    # Add the predicted actions as a new column to the DataFrame
+    # Adjust this part based on your DataFrame's structure
+    df[('Action', currency)] = predicted_actions
 
-    # Convert the state to PyTorch tensor and transfer to the device (GPU/CPU)
-    current_state_tensor = torch.from_numpy(current_state).float().to(agent.device)
+    return df
 
-    action = agent.act(current_state_tensor)
-    predicted_actions.append(action)
+predicted_df_train = make_predictions_with_agent(agent, df_train, look_back, currencies[0])
+predicted_df_test = make_predictions_with_agent(agent, df_test, look_back, currencies[0])
 
-padding = [None] * look_back
-full_predictions = padding + predicted_actions
+win_rate_train,  number_of_trades_train = calculate_win_rate(predicted_df_train, currencies[0])
+print(f"Win Rate train: {win_rate_train * 100:.2f}%")
+print(f"Number of trades train: {number_of_trades_train}")
+win_rate_test,  number_of_trades_test = calculate_win_rate(predicted_df_test, currencies[0])
+print(f"Win Rate test: {win_rate_test * 100:.2f}%")
+print(f"Number of trades test: {number_of_trades_test}")
 
-df_train['Predicted_Action'] = full_predictions
+from backtest.backtest_functions.backtest import Strategy
+
+class SimpleDQN(Strategy):
+    def __init__(self, currencies, agent, look_back, leverage=1.0, provision=0.0001, starting_capital=10000):
+        super().__init__(currencies, leverage, provision, starting_capital)
+        self.agent = agent
+        self.look_back = look_back
+
+    def calculate_new_positions(self, df, current_index):
+        if isinstance(current_index, pd.Timestamp):
+            current_index = df.index.get_loc(current_index)
+        new_positions = {}
+        for currency in self.currencies:
+            # Ensure the current_index is within the bounds of the dataframe
+            if current_index >= self.look_back:
+                state = df[variables].iloc[current_index - self.look_back:current_index].values.flatten().reshape(1, -1)
+                action = agent.epsilon_greedy_policy(state)
+                # Get the available capital at the current index
+                available_capital = df.loc[df.index[current_index], 'Available_Capital'].iloc[0]
+
+                if action == 1:
+                    # Going long with available capital
+                    new_positions[currency] = available_capital
+                elif action == -1:
+                    # Going short with available capital
+                    new_positions[currency] = -available_capital
+                else:
+                    # Doing nothing, no position
+                    new_positions[currency] = 0
+            else:
+                new_positions[currency] = 0
+
+        return new_positions
+
+# Usage Example
+DQN_strategy = SimpleDQN(currencies, agent=agent, look_back=look_back, starting_capital=10000)
+predicted_df_test = DQN_strategy.backtest(predicted_df_test)
+
+report_df_positions = DQN_strategy.generate_report(predicted_df_test)
+report = DQN_strategy.generate_extended_report()
+DQN_strategy.display_summary_as_table(predicted_df_test, extended_report=True)
+
+# Plotting
+from backtest.plots.plot import plot_financial_data
+
+plot_financial_data(predicted_df_test, DQN_strategy, currencies, volatility='garman_klass_volatility', n=100)
+
--- technical_analysys/indicators.py
+++ technical_analysys/indicators.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import warnings
 from tqdm import tqdm
+import pandas as pd
 
 def RSI(series, length=14):
     """Compute the RSI (Relative Strength Index) of the provided series."""
@@ -22,3 +23,41 @@ def EMA(series, length=14):
     """Compute the EMA (Exponential Moving Average) of the provided series."""
     ema = series.ewm(span=length, adjust=False).mean()
     return ema
+
+
+def calculate_rsi(data, window=14):
+    delta = data.diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
+    rs = gain / loss
+    return 100 - (100 / (1 + rs))
+
+def calculate_macd(data, slow=26, fast=12, signal=9):
+    exp1 = data.ewm(span=fast, adjust=False).mean()
+    exp2 = data.ewm(span=slow, adjust=False).mean()
+    macd = exp1 - exp2
+    return macd
+
+def calculate_atr(high, low, close, window=14):
+    high_low = high - low
+    high_close = np.abs(high - close.shift())
+    low_close = np.abs(low - close.shift())
+    ranges = pd.concat([high_low, high_close, low_close], axis=1)
+    true_range = np.max(ranges, axis=1)
+    return true_range.rolling(window=window).mean()
+
+def calculate_stochastic_oscillator(high, low, close, k_window=14, d_window=3):
+    lowest_low = low.rolling(window=k_window).min()
+    highest_high = high.rolling(window=k_window).max()
+    k = ((close - lowest_low) / (highest_high - lowest_low)) * 100
+    d = k.rolling(window=d_window).mean()
+    return d - k
+
+def calculate_ultosc(high, low, close):
+    # This is a simplified version. The actual calculation may be more complex.
+    bp = close - low
+    tr = high - low
+    avg_bp = bp.rolling(window=7).mean()
+    avg_tr = tr.rolling(window=7).mean()
+    ultosc = 100 * (avg_bp / avg_tr)
+    return ultosc
