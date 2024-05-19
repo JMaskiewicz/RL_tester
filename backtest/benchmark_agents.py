those results are too good, fixed bug with average trade duration

backtest/benchmark_agents.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     np.random.seed(0)
     random.seed(0)
 
-    tradable_markets = 'USDJPY'
+    tradable_markets = 'EURUSD'
     starting_balance = 10000
     look_back = 1
     provision = 0.0001
@@ -145,7 +145,7 @@ if __name__ == '__main__':
     #df_test = pd.concat([df_validation.iloc[-look_back:], df_test])
 
     variables = [
-        {"variable": ("Close", "USDJPY"), "edit": "standardize"},
+        {"variable": ("Close", "EURUSD"), "edit": "standardize"},
     ]
 
     buy_and_hold_agent = Buy_and_hold_Agent()
@@ -187,8 +187,8 @@ if __name__ == '__main__':
     print("Perfect Hold Agent final results:", ph_results_prepared[('PH', 'Final Balance')][0])
     print("Benchmark Perfect Hold Agent final reward:", ph_results_prepared[('PH', 'Total Reward')][0])
 
-    first_close = df_test.loc[:, ('Close', 'USDJPY')].iloc[look_back]
-    last_close = df_test.loc[:, ('Close', 'USDJPY')].iloc[-1]
+    first_close = df_test.loc[:, ('Close', 'EURUSD')].iloc[look_back]
+    last_close = df_test.loc[:, ('Close', 'EURUSD')].iloc[-1]
     return_percentage = ((last_close - first_close) / first_close) * 100
 
     # Calculate the final investment value